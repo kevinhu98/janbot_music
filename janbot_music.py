9231d6bde35bace43c387419090999a925071af7
Kevin Hu <kh2547@nyu.edu>
2023-12-23 21:59:46 -0500

    added option to play by keywords instead of link

--- janbot_music.py
+++ janbot_music.py
@@ -33,7 +33,7 @@ async def join(ctx):
 
 # command to play sound from a youtube URL
 @client.command()
-async def play(ctx, url):
+async def play(ctx, *url):
     YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
     FFMPEG_OPTIONS = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
@@ -41,11 +41,20 @@ async def play(ctx, url):
 
     if not voice.is_playing():
         with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-        URL = info['url']
+            url = " ".join(url)
+            # case if url is a link 
+            if "https://www.youtube.com" in url:
+                info = ydl.extract_info(url, download=False)
+                URL = info['url']
+                title = info['title']
+            # case if url is keywords
+            else:
+                info = ydl.extract_info("ytsearch:{url}".format(url=url), download=False)
+                URL = info['entries'][0]['url']
+                title = info['entries'][0]['title']
         voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
         voice.is_playing()
-        await ctx.send('Bot is playing')
+        await ctx.send('Janbot is playing {title}'.format(title=title))
 
 # check if the bot is already playing
     else:
